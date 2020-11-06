0c9000f3da300d2d4edc7c7b767947f775be5ce3
cwinters8 <clwinters8@gmail.com>
2020-11-05 19:53:37 -0600

    added a DELETE method and an explanation for json.dumps

--- server/api/mentions_handler.py
+++ server/api/mentions_handler.py
@@ -3,11 +3,11 @@ from datetime import datetime
 from pymongo import MongoClient
 from bson import json_util
 import json
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 mentions_handler = Blueprint('mentions_handler', __name__)
 
 
-@mentions_handler.route('/mentions', methods=['GET', 'POST'])
+@mentions_handler.route('/mentions', methods=['GET', 'POST', 'DELETE'])
 def mentions():
     connection_string = 'mongodb+srv://{username}:{password}@{server}/?retryWrites=true&w=majority'
     client = MongoClient(connection_string.format(
@@ -27,9 +27,14 @@ def mentions():
             'date': datetime.now(),
             'popularity': 1
         })
-        return result.inserted_id
+        return str(result.inserted_id)
+    elif request.method == 'DELETE':
+        result = mentions_collection.delete_many({})
+        return jsonify({'documentsDeleted': result.deleted_count})
     elif request.method == 'GET':
         mentions = []
         for mention in mentions_collection.find():
             mentions.append(mention)
+        # using json.dumps here instead of jsonify
+        # needed a way to pass a default type for types that could not be converted to JSON
         return json.dumps({'mentions': mentions}, default=json_util.default)
